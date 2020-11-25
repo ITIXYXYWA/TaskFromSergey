48c0d00c2dec05539bdbe5bf80bb5c5fc6a7b16b
ITIXYXYWA <ukv220@gmail.com>
2020-11-25 18:29:39 +0500

    vers 2

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
+    let timeWork = document.getElementById('timeWork'); // время работы 
+    
+    let turn = document.getElementById('turn'); // включение/выключение проектора
+
+    let speed = document.getElementById('speed'); // скорость vom
+    let timeOpen = document.getElementById('timeOpen'); // время открытия экрана
+    let timeClose  = document.getElementById('timeClose'); // время закрытия экрана
+    
+
+    let mcrStep = document.getElementById('mcrStep'); // шаг микрофона 
+    let mcrMax = document.getElementById('mcrMax'); //если неккоректно высставить мин значение(если пришло неверные значения от контроллерра то на мин поставить)
+    let mcrMin = document.getElementById('mcrMin'); // мин. значение микро
+
+    let volStep = document.getElementById('volStep'); // шаг звука
+    let volMin = document.getElementById('volMin'); // минимальная громкость 
+    let volMax = document.getElementById('volMax'); // макс громкость 
+
+    let settingObj = {};  // объект с данными
+
     document.body.onload = function () {
         let xhr = new XMLHttpRequest();
         xhr.open('POST', '', true);
 
         xhr.onload = function () {
-            let settingFromController = JSON.stringify(xhr.response);
-            let k;
-            for (k in settingFromController) {
-            }
+            let SFT = JSON.parse(xhr.response);
+            
         }
         xhr.send();
     }
-
-    let timeWork = 1234312312; // время работы 
-    let turn = 3; // включение/выключение ПК
-
-    let speed = 13414; // скорость vom
-    let timeClose  = 1242144; // время закрытия проектора
-    let timeOpen = 3254235235; // время открытия проектора
-
-    let mcrStep = 3143; // шаг микрофона 
-    let mcrMin = 14124; // мин. значение микро
-    let mcrMax = 250; //если неккоректно высставить мин значение(если пришло неверные значения от контроллерра то на мин поставить)
-
-    let volumeStep = 12412; // шаг звука
-    let volumeMin = 6541; // минимальная громкость 
-    let volumeMax = 823823; // макс громкость 
-
-    let settingObj = {};  // объект с данными
 
     function check (x,y) {
         if(typeof(x) === 'number'){ 
@@ -46,17 +46,17 @@
         check(mcrStep,'mcrStep');
         check(mcrMin, 'mcrMin');
         check(mcrMax, 'mcrMax');
-        check(volumeStep, 'volumeStep');
-        check(volumeMin, 'volumeMin');
-        check(volumeMax, 'volumeMax');
+        check(volStep, 'volumeStep');
+        check(volMin, 'volumeMin');
+        check(volMax, 'volumeMax');
 
         console.log(settingObj);
 
-        // let xhr = new XMLHttpRequest();
+        let xhr = new XMLHttpRequest();
 
-        // xhr.open('POST', '', true);
+        xhr.open('POST', '', true);
 
-        // xhr.send(settingObj);
+        xhr.send(JSON.stringify(settingObj));
     }
 
 
